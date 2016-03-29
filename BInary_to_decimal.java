8fdd5275e2594545e5ec98ebd962de72db3e00ac
V.GURU PRASAD <vguruprasad91@gmail.com>
2016-03-29 10:56:52 -0400

    Update BInary_to_decimal.java

--- BInary_to_decimal.java
+++ BInary_to_decimal.java
@@ -11,13 +11,13 @@ class Bintodec {
     Scanner input = new Scanner( System.in );
     System.outprint("Enter the number of test cases :");
     String T = input.nextLine();
-    if ( T>100 || T<1 ){
+    if ( Integer.parseInt(T) >100 || Integer.parseInt(T)<1 ){
       System.out.println("Enter the number of testcases between 1 to 100");
     }
-    for ( i=0; i<T; i++){
+    for ( i=0; i<Integer.parseInt(T); i++){
     System.out.print("Enter a binary no :");
     String binaryString = input.nextLine();
-    if (strlen(binaryString) >8 ){
+    if (binaryString.length() >8 ){
       System.out.println("Enter the length of binary string to be between 1 to 8");
     }
     System.out.println("Output: "+Integer.parseInt(binaryString, 2));
