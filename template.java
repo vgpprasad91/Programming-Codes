58e6d6e94a23ca50e4eb268210e94158dce710f4
V.GURU PRASAD <vguruprasad91@gmail.com>
2016-03-29 11:25:05 -0400

    Added template for T-N-A
    Added template for Testcase-Number-Array

--- template.java
+++ template.java
@@ -37,8 +37,30 @@ class template {
         if ( Integer.parseInt(N) >1000 || Integer.parseInt(N)<1 ){
           System.out.println("Enter the number of testcases between 1 to 100");
     } 
+  } 
+} 
+
+// template for T-N-A
+
+// template for getting a number within a testcase ( T-N || T-N-A)
+
+import java.util.Scanner;
+class template { 
+  public static void main(String args[]){
+    Scanner input = new Scanner( System.in );
+    System.out.println("Enter the number of test cases :");
+    String T = input.nextLine();
+    if ( Integer.parseInt(T) >100 || Integer.parseInt(T)<1 ){
+      System.out.println("Enter the number of testcases between 1 to 100");
+    } 
+    for ( i=0; i<Integer.parseInt(T); i++){
+        System.out.print("Enter the number :");
+        String N = input.nextLine();
+        if ( Integer.parseInt(N) >1000 || Integer.parseInt(N)<1 ){
+          System.out.println("Enter the number of testcases between 1 to 100");
+    } 
         for ( i=0; i<Integer.parseInt(N); i++){
-          
+          String N = input.nextLine();
     } 
   } 
 } 
